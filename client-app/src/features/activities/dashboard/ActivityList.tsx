113f7955eab8ab170c84436757fd2925656f3e06
Pieter Jan Geutjens <pieter.geutjens@leapconsulting.be>
2021-08-13 18:49:17 +0200

    styling

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,41 +1,26 @@
 import { observer } from 'mobx-react-lite'
-import React, { SyntheticEvent, useState } from 'react'
-import { NavLink } from 'react-router-dom'
-import { Button, Item, Label, Segment } from 'semantic-ui-react'
+import { Fragment } from 'react'
+import { Header } from 'semantic-ui-react'
 import { useStore } from '../../../app/stores/store'
+import ActivityListItem from './ActivityListItem'
 
 export default observer(function ActivityList() {
-    const {activityStore} = useStore()
-    const {activitiesByDate, deleteActivity, loading} = activityStore
-
-    const [target, setTarget] = useState('')
-
-    function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string) {
-        setTarget(e.currentTarget.name)
-        deleteActivity(id)
-    }
+    const { activityStore } = useStore()
+    const { groupedActivities } = activityStore
 
     return (
-        <Segment>
-            <Item.Group>
-                {activitiesByDate.map(activity => (
-                    <Item key={activity.id}>
-                        <Item.Content>
-                            <Item.Header as='a'>{activity.title}</Item.Header>
-                            <Item.Meta>{activity.date}</Item.Meta>
-                            <Item.Description>
-                                <div>{activity.description}</div>
-                                <div>{activity.city}, {activity.venue}</div>
-                            </Item.Description>
-                            <Item.Extra>
-                                <Button floated='right' as={NavLink} to={`/activities/${activity.id}`} content='View' color='blue' />
-                                <Button name={activity.id} loading={loading && target === activity.id} floated='right' onClick={(e) => handleActivityDelete(e, activity.id)} content='Delete' color='red' />
-                                <Label basic content={activity.category} />
-                            </Item.Extra>
-                        </Item.Content>
-                    </Item>
-                ))}
-            </Item.Group>
-        </Segment>
+        <>
+            {groupedActivities.map(([group, activities]) => (
+                <Fragment key={group}>
+                    <Header color='teal'>
+                        {group}
+                    </Header>
+                    {activities.map(activity => (
+                        <ActivityListItem activity={activity} key={activity.id} />
+                    ))}
+                </Fragment>
+            ))}
+        </>
+
     )
 })
--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,10 +1,13 @@
 import { observer } from 'mobx-react-lite'
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import { useParams } from 'react-router'
-import { Link } from 'react-router-dom'
-import { Button, Card, Image } from 'semantic-ui-react'
+import { Grid } from 'semantic-ui-react'
 import LoadingComponent from '../../../app/layout/LoadingComponent'
 import { useStore } from '../../../app/stores/store'
+import ActivityDetailChat from './ActivityDetailChat'
+import ActivityDetailHeader from './ActivityDetailHeader'
+import ActivityDetailInfo from './ActivityDetailInfo'
+import ActivityDetailSidebar from './ActivityDetailSidebar'
 
 export default observer (function ActivityDetails() {
     const {activityStore} = useStore()
@@ -19,23 +22,15 @@ export default observer (function ActivityDetails() {
     if (loadingInitial || !activity) return (<LoadingComponent content=''/>);
 
     return (
-        <Card fluid>
-            <Image src={`/assets/categoryImages/${activity.category}.jpg`} />
-            <Card.Content>
-                <Card.Header>{activity.title}</Card.Header>
-                <Card.Meta>
-                    <span>{activity.date}</span>
-                </Card.Meta>
-                <Card.Description>
-                    {activity.description}
-                </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-                <Button.Group widths='2'>
-                    <Button as={Link} to={`/manage/${activity.id}`} basic color='blue' content='Edit' />
-                    <Button as={Link} to={'/activities'}basic color='grey' content='Cancel' />
-                </Button.Group>
-            </Card.Content>
-        </Card>
+        <Grid>
+            <Grid.Column width={10}>
+                <ActivityDetailHeader activity={activity} />
+                <ActivityDetailInfo activity={activity} />
+                <ActivityDetailChat />
+            </Grid.Column>
+            <Grid.Column width={6}>
+                <ActivityDetailSidebar/>
+            </Grid.Column>
+        </Grid>  
     )
 })
