adde39b56a06acc1c8c39b3c7e4aac72812b9af8
Pieter Jan Geutjens <pieter.geutjens@leapconsulting.be>
2021-08-30 11:58:54 +0200

    emailconfirmation complete

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -51,7 +51,7 @@ axios.interceptors.response.use(async response => {
             }
             break;
         case 401:
-            if (status === 401 && headers['www-authenticate'].startsWith('Bearer error="invalid_token"')) {
+            if (status === 401 && headers['www-authenticate']?.startsWith('Bearer error="invalid_token"')) {
                 store.userStore.logout();
                 toast.error('Session expired - please login again');
             }
@@ -90,7 +90,11 @@ const Account = {
     current: () => requests.get<User>('/account'),
     login: (user: UserFormValues) => requests.post<User>('account/login', user),
     register: (user: UserFormValues) => requests.post<User>('account/register', user),
-    refreshToken: () => requests.post<User>('/account/refreshToken', {})
+    refreshToken: () => requests.post<User>('/account/refreshToken', {}),
+    verifyEmail: (token: string, email: string) => 
+        requests.post<void>(`/account/verifyEmail?token=${token}&email=${email}`, {}),
+    resendEmailConfirm: (email: string) => 
+    requests.post<void>(`/account/resendEmailConfirmationLink?email=${email}`, {}),
 }
 
 const Profiles = {
